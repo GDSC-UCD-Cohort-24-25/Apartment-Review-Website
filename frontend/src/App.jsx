77b27556dfb8ff4bc8faa9bc6d40ef96c116ccd0
jamiep <yeojinp2458@gmail.com>
2025-02-19 19:11:20 -0800

    sd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,43 @@
-import { useState } from 'react'
-import './App.css'
-import Home from "./pages";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './App.css';
 
 function App() {
-
   return (
     <Router>
-      <Navbar />
-      
-    <Router>
+      <nav className="navbar">
+        <ul className="navbar-links">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
+}
+
+function Home() {
+  return <h1>Welcome to the Home Page</h1>;
+}
+
+function About() {
+  return <h1>About Us</h1>;
+}
+
+function Contact() {
+  return <h1>Contact Us</h1>;
 }
 
 export default App;
