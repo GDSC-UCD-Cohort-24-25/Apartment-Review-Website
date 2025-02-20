3281bf0fce075535f6508e2e7b44838db2180131
jamiep <yeojinp2458@gmail.com>
2025-02-19 19:44:21 -0800

    sdf

--- frontend/src/navbar.jsx
+++ frontend/src/navbar.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './App.css';
 
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
-      <li><a href="/about.jsx">Home</a></li>
-      <li><a href="/officer">About</a></li>
-      <li><a href="/officer">Contact</a></li>
-      <li><a href="/officer">Apartments</a></li>
+      <li>
+          <Link to="/">
+            <button className="nav-button">Home</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/about">
+            <button className="nav-button">About</button>
+          </Link>
+        </li>
+        <li>
+          <Link to="/contact">
+            <button className="nav-button">Contact</button>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
