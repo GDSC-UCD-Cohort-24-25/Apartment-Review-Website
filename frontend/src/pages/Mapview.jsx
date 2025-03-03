9a87ad64c448f0c1673652a5f966221996c9c266
Saarth09 <Saarth0909@gmail.com>
2025-03-03 01:23:02 -0800

    map page semi done

--- frontend/src/pages/Mapview.jsx
+++ frontend/src/pages/Mapview.jsx
@@ -1,11 +1,47 @@
 import React from 'react';
 import './Mapview.css';
 
-
 const Map = () => {
   return (
-    <div>
-      <h1>Map View Page</h1>
+    <div className="map-main-content">
+      {/* Listings */}
+      <div className="map-listings">
+        <h2>Map View</h2>
+        <div className="map-listing">
+          <img src='apartments/thedrakeandanderson/the-drake-and-anderson-court-davis-ca-primary-photo.jpg' alt="Drake and Anderson" />
+          <h3>Drake and Anderson</h3>
+          <p>West Davis Manor</p>
+          <p>650/month</p>
+          <div className="map-rating">
+            <i className="fas fa-star"></i> 4.5
+          </div>
+        </div>
+        <div className="map-listing">
+          <img src="apartments/almondwood/almondwood-apartments-20200519-064.jpg" alt="Almond Apartments" />
+          <h3>Almond Apartments</h3>
+          <p>West Davis Manor</p>
+          <p>650/month</p>
+          <div className="map-rating">
+            <i className="fas fa-star"></i> 4.5
+          </div>
+        </div>
+      </div>
+
+      {/* Map */}
+      <div className="map-map">
+        <img src="https://storage.googleapis.com/a1aa/image/i1aPjXTarkfZFtmDfnGl1j4vcBUh7poVXnOSG_nJXcU.jpg" alt="Map" />
+        <div className="map-map-prices">
+          <div className="map-price">$74</div>
+          <div className="map-price">$177</div>
+          <div className="map-price">$108</div>
+          <div className="map-price">$69</div>
+          <div className="map-price">$91</div>
+          <div className="map-price">$130</div>
+          <div className="map-price">$59</div>
+          <div className="map-price">$114</div>
+          <div className="map-price">$19</div>
+        </div>
+      </div>
     </div>
   );
 };
