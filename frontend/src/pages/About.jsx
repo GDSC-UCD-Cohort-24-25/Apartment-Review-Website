54921cb2bb949f95f6c60e6890eb22b3f7d9123b
jamiep <yeojinp2458@gmail.com>
2025-03-06 16:21:36 -0800

    f

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -3,8 +3,12 @@ import './About.css';
 
 function About() {
   return (
-    <div>
+    <div className="about-container">
       <h1>About Us</h1>
+      <h4>This is Daving Housing Website!</h4>
+      <p>Lorem ipsum odor amet, consectetuer adipiscing elit. Placerat feugiat posuere pulvinar erat mattis praesent. Odio fames porta mattis parturient mattis eleifend. Cubilia diam felis nibh aenean id rutrum. Porttitor curabitur rhoncus integer sociosqu nascetur vulputate lorem convallis efficitur. Ullamcorper curae nostra inceptos augue primis ante posuere. Aet vel tellus maximus potenti proin hac. Justo litora tempus non in tincidunt sagittis phasellus. Ex tristique mi nibh lacinia; dignissim orci inceptos eget.
+
+ Commodo efficitur dis molestie euismod nisi aliquam. Porta eget auctor nec proin hac, in leo. Feugiat et bibendum hendrerit urna curabitur; feugiat vel pretium. Ridiculus dictumst mus volutpat per ornare adipiscing. Convallis blandit curae facilisis arcu dui tempor. Condimentum suspendisse enim finibus accumsan convallis, tempor placerat vivamus diam. Montes finibus rhoncus felis mus ultricies fringilla nam primis. Nisi fermentum fames morbi accumsan eleifend tempus duis nam bibendum? Quisque laoreet habitasse ornare porta nibh penatibus.</p>
     </div>
   );
 }
